3bf836b9d7de5cecdcf1906895e00cd64e22971e
Ara <ayaghsizian@gmail.com>
2017-12-02 13:54:24 -0500

    cannot use ildasm (intermediate language disassembler for some reason) will try again later

--- cSharp-Intermediate/Program.cs
+++ cSharp-Intermediate/Program.cs
@@ -9,35 +9,14 @@ namespace cSharp_Intermediate
 
     public class Person
     {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}", to, Name);
-        }
-
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-            return person;
-
-
-
-        }
+        public DateTime Birthdate { get; set; }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            // Because we made Parse a static method
-            var bob = Person.Parse("Bob");
-            bob.Introduce("Ara");
-
-            //var bob = new Person();
-            //bob.Name = "Bob";
-            //bob.Introduce("Ara");
+            
         }
     }
 }
